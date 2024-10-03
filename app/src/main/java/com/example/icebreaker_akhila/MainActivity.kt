b25ab16e23559b60134d693136ed235eae15f97d
Akhila Polineni <akhilapolineni11@gmail.com>
2024-10-03 10:25:47 -0400

    Linked view to controller

--- app/src/main/java/com/example/icebreaker_akhila/MainActivity.kt
+++ app/src/main/java/com/example/icebreaker_akhila/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.view.LayoutInflater
 import com.example.icebreaker_akhila.databinding.ActivityMainBinding
+import android.nfc.Tag
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,7 +25,16 @@ class MainActivity : AppCompatActivity() {
         }
         binding.btnSubmit.setOnClickListener{
             binding.txtQuestion.text = ""
+            writeStudentToFirebase()
 
         }
+    }
+    private fun writeStudentToFirebase(){
+        val firstName = binding.txtFirstName.text
+        val lastName = binding.txtLastName.text
+        val prefName = binding.txtPrefName.text
+        val answer = binding.txtAnswer.text
+
+        Log.d("IcebreakerF24", "Variables: $firstName $lastName $prefName $answer")
     }
 }
